30ec7fa19515cc4a8b6c5db14c0dece46c6b1cf9
Apple <apple@Apples-MacBook-Pro.local>
2024-03-11 21:00:58 +0530

    search description that highlight by yellow color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [listOfProducts, setListOfProducts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const searchedFiledHighlighted = (text) => {
+    if (!searchTerm) return text;
+
+    const index = text.toLowerCase().indexOf(searchTerm.toLowerCase());
+    if (index === -1) return text;
+
+    return (
+      <>
+        {text.substring(0, index)}
+        <span style={{ backgroundColor: "yellow" }}>
+          {text.substring(index, index + searchTerm.length)}
+        </span>
+        {text.substring(index + searchTerm.length)}
+      </>
+    );
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => {
+        setLoading(true);
+        setListOfProducts(res.data.products);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log("🚀 ~ useEffect ~ err:", err);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Please Search..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ width: "80%", padding: "20px", margin: "10px" }}
+      />
+      {loading ? (
+        <p>Loading products...</p>
+      ) : (
+        <ul>
+          {listOfProducts.map((product) => {
+            return (
+              <li style={{ textAlign: "left" }}>
+                {searchedFiledHighlighted(product.description)}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
